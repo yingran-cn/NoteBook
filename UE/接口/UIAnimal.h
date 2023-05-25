2a9d0f2966cf759c0bb27ef96412871c28998a3e
yingran-cn <yingran@live.cn>
2023-05-26 01:28:16 +0800

    Create UIAnimal.h

---
+++ UE/接口/UIAnimal.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IAnimal.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UIAnimal : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MYPROJECT_API IIAnimal
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	virtual FString Say();
+};
